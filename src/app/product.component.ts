d720f4e8973456a050a5270f1d0dae53ea588544
Felly Kanyiki <fellyka@gmail.com>
2025-02-23 16:25:03 +0200

    Use of signal in Angular

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Model } from './repository.model';
 
 @Component({
@@ -8,23 +8,28 @@ import { Model } from './repository.model';
 export class ProductComponent {
   private model: Model = new Model();
   private  messages = ["Total","Price"];
-  private index = 0;
+  private index = signal<number>(0);
 
   get count(): number {
     let result = this.model.getProducts().length;
+    // let total = 0;
+    //     for (let i = 0; i < 1000000000; i++) {
+    //       total += i;
+    //     }
     console.log(`count value read: ${result}`);
     return result;
       }
 
-      get total(): string {
-        let result = this.model.getProducts()
-           .reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2);
+      get total(): number {
+        let result = this.model.getProducts().length;
+
            console.log(`total value read: ${result}`);
            return result;
       }
 
   get message(): string {
-    let result = `${this.messages[this.index]}: ${this.total}`;
+    /*The value of a signal is read by invoking the signal as a function, like: this.index()*/
+    let result = `${this.messages[this.index()]}: ${this.total}`;
     console.log(`message value read: ${result}`);
     return result;
   }
@@ -33,7 +38,13 @@ export class ProductComponent {
   toggleMessage(){
     console.clear();
     console.log(`message value read: ${this.message}`);
-    this.index = (this.index +1) % 2;
+   // this.index = (this.index +1) % 2;
+   /*Unlike a normal Javascript value, signals are not modified using the assignment operator.
+     Instead, signals are modified using one the following methods: ste=>accept a new value for the signal,
+     update => accepts a fx that receives the current signal value and returns a new vale, mutate => this method
+     accepts a fx that receives the current signal value and modifies it in place
+     asReadonly =>a method that returns Signal<T> object, which provides a read-only version of the signal.*/
+   this.index.update(currentVal => (currentVal + 1) % 2);
   }
 
   removeProduct() {
