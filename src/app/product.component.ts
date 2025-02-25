bfb3a977f31339ab881939360afb077d4c0dab3f
Felly Kanyiki <fellyka@gmail.com>
2025-02-25 21:01:32 +0200

    How to use If statement in Angular

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -11,23 +11,15 @@ export class ProductComponent {
   private model: Model = new Model();
 
  products = computed<Product[]>(() => this.model.getProducts());
- count = computed<number>(() => this.model.getProducts().length);
+  count = computed<number>(() => this.model.getProducts().length);
 
- classes = computed<string>( ()=>
-   this.count() === 5 ? "bg-success" : "bg-warning")
 
- someInfo: string = "bg-info text-white text-center";
-
- sayHello():void{
-    console.log("Hello");
- }
-
- getClasses(key: number) {
-  return "p-2 " + (((this.products()[key].price ?? 0) > 50)
-  ? "bg-info" : "bg-warning");
+ product(key: number): Product | undefined {
+  return this.model.getProduct(key);
   }
-
- Name = "Product Component";
+  removeProduct() {
+  this.model.deleteProduct(this.model.getProducts()[0].id ?? 0);
+  }
 
 }
 
